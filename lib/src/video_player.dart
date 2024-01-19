a2da9a246b237ee3422aa95dfb86e2b95ae6d1ad
Sebastian Schneider <sebastian.schneider@simpleclub.com>
2024-01-19 12:29:17 +0100

    Fix video buffering crashing on slow networks

--- lib/src/video_player.dart
+++ lib/src/video_player.dart
@@ -107,13 +107,17 @@ class VideoPlayer {
           _hls!.loadSource(uri.toString());
         }));
         _hls!.on('hlsError', allowInterop((dynamic _, dynamic data) {
-          final ErrorData _data = ErrorData(data);
-          if (_data.fatal) {
-            _eventController.addError(PlatformException(
-              code: _kErrorValueToErrorName[2]!,
-              message: _data.type,
-              details: _data.details,
-            ));
+          try {
+            final ErrorData _data = ErrorData(data);
+            if (_data.fatal) {
+              _eventController.addError(PlatformException(
+                code: _kErrorValueToErrorName[2]!,
+                message: _data.type,
+                details: _data.details,
+              ));
+            }
+          } catch (e) {
+            debugPrint('Error parsing hlsError: $e');
           }
         }));
         _videoElement.onCanPlay.listen((dynamic _) {
